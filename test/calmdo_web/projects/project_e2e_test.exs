c9c8f37f27710fc0d3ecfd658e50a9e1743ad072
Zaseem-BIsquared <zaseem@bisquared.com>
2025-12-15 15:31:26 +0530

    RED: basic e2e test for projects

---
+++ test/calmdo_web/projects/project_e2e_test.exs
@@ -0,0 +1,30 @@
+defmodule CalmdoWeb.Projects.ProjectE2eTest do
+  use CalmdoWeb.ConnCase, async: true
+
+  describe "project e2e" do
+    test "project CRUD", %{conn: conn} do
+      conn
+      |> visit(~p"/")
+      # create a project
+      |> click_link("Create Project")
+      |> fill_in("Name", with: "Test Project 1")
+      |> submit()
+      |> assert_text("Test Project 1")
+
+      # show a project
+      |> click_link("Test Project 1")
+      |> assert_has("main", text: "Test Project 1")
+
+      # update a project
+      |> click_link("Edit")
+      |> fill_in("Name", with: "Test Project 2")
+      |> submit()
+      |> assert_text("Test Project 2")
+
+      # delete a project
+      |> click_link("Edit")
+      |> click_link("Delete")
+      |> refute_text("Test Project 2")
+    end
+  end
+end
