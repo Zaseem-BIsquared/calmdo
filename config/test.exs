c496aab83860f9d0ebfff443c4384264a15665db
Zaseem-BIsquared <zaseem@bisquared.com>
2025-12-15 15:13:53 +0530

    🔥 initial commit 🔥

---
+++ config/test.exs
@@ -0,0 +1,41 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :calmdo, Calmdo.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "calmdo_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :calmdo, CalmdoWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "YFWX/DQJSQ7aRb6Trv3zy9DM5Ay+sua0Teydso9F0aLbBBepZcwT3Pdeu0ox6b4b",
+  server: false
+
+# In test we don't send emails
+config :calmdo, Calmdo.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
+
+# Sort query params output of verified routes for robust url comparisons
+config :phoenix,
+  sort_verified_routes_query_params: true
